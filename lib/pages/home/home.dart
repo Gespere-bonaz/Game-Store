22af24c449201b3726fdc413ef1a4507b5be9eb5
Gespere-bonaz <gloirebonaz@gmail.com>
2025-01-30 17:08:41 +0100

    page header

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -39,26 +46,62 @@ class HomePage extends StatelessWidget {
   }
 
   Widget navigationBar() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: [
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home),
+    return Container(
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withAlpha(51),
+          spreadRadius: 5,
+          blurRadius: 10,
+        )
+      ]),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
         ),
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home),
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          selectedItemColor: const Color(0xFF5F67EA),
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          unselectedItemColor: Colors.grey.withAlpha(51),
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _buildNavItem(Icons.home, 'Home', 0),
+            _buildNavItem(Icons.apps, 'Application', 1),
+            _buildNavItem(Icons.movie, 'Film', 2),
+            _buildNavItem(Icons.auto_stories_rounded, 'Book', 3),
+          ],
         ),
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem(
+      IconData icon, String label, int index) {
+    return BottomNavigationBarItem(
+      label: label,
+      icon: Container(
+        margin: const EdgeInsets.all(5),
+        padding: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? const Color(0xFF5F67EA).withAlpha(51)
+              : Colors.grey.withAlpha(51),
+          borderRadius: BorderRadius.circular(10),
         ),
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home),
+        child: Icon(
+          icon,
+          size: 30,
+          color:
+              _selectedIndex == index ? const Color(0xFF5F67EA) : Colors.grey,
         ),
-      ],
+      ),
     );
   }
 }
